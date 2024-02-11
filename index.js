10b464bcb764c70da89dd4693c0f5779ca36f19f
Pravin190 <106483762+Pravin190@users.noreply.github.com>
2024-02-11 21:29:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,25 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-
 const PORT = process.env.PORT || 4000;
 
 // cookie-parser to fetch token
 const cookieParser = require("cookie-parser");
 app.use(cookieParser()); 
-
 app.use(express.json());
 
-//database import
-
+//database import and Establish DB Connection
 const dbconnect = require("./config/database");
-
 dbconnect();
 
 //routes import
-
 const user = require("./routes/user");
-
 app.use("/api/v1",user);
 
-//activate
-
+//activate Server
 app.listen(PORT,() => {
-
     console.log(`server run at ${PORT}`);
-
 })
-
-// default route
 
 app.get("/",(req,res) => {
 
